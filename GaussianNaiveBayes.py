0f81fe7a4a618fb0f94015dd3476b82086093f0d
Aansh Basu <96300383+Aansh2003@users.noreply.github.com>
2023-03-17 15:56:03 +0530

    Update GaussianNaiveBayes.py

--- GaussianNaiveBayes.py
+++ GaussianNaiveBayes.py
@@ -5,35 +5,30 @@ import numpy as np
 # df = dataframe with pre-processed data(type=pandas.dataframe)
 # target = label of the target attribute(type=string)
 # pred = test case values(type=list)
+# A package to implement a Gaussian predictive algorithm.
+import pandas as pd
+import numpy as np
+
+# df = dataframe with pre-processed data(type=pandas.dataframe)
+# target = label of the target attribute(type=string)
+# pred = test case values(type=list)
 class GaussianPrediction:
-    def GaussianPredictor(self,df,target,pred=[]):
-        # All error cases handling
-        if(not (type(df))==pd.core.frame.DataFrame):
-            print('ERROR: Argument \'df\' must be of type pandas.core.frame.DataFrame')
-            exit()
-        if(not type(target)==str):
-            print('ERROR: argument \'target\' must be of type str')
-            exit()
-        if(not target in df.columns):
-            print('ERROR: target: \''+target+'\' not in dataframe')
-            exit()
-        if(len(df.columns)-1 != len(pred)):
-            print('ERROR: Incorrect size of \'pred\'')
-            exit()
-        df_vars = df.drop(target,axis=1)
-        df_target = df[target]
-        i = 0
-        final_probs = []
-        for value in df_vars:
-            final_probs.append(self.GaussianProbability(df_vars[value],pred[i]))
-            i = i+1
-        arr = []
-        for value in final_probs:
-            arr.append(self.inverseGaussianCalc(df_target,value))
-        print("Prediction: ")
-        print(np.mean(arr))
-        print("Probability: ")
-        print(np.mean(final_probs)*100)
+    def predict(self,X_test):
+        y_new = []
+        for k in range(len(X_test)):
+            i = 0
+            final_probs = []
+            for value in self.X:
+                final_probs.append(self.GaussianProbability(self.X[value],X_test.iloc[k][value]))
+            arr = []
+            for value in final_probs:
+                arr.append(self.inverseGaussianCalc(Y,value))
+            y_new.append(np.mean(arr))
+        return y_new
+            
+    def fit(self,X,Y,pred=[]):
+        self.X = X
+        self.Y = Y
 
     def multiStandardDeviation(self,df):
         return df.std()
@@ -49,5 +44,4 @@ class GaussianPrediction:
     def inverseGaussianCalc(self,df,prob):
         s = self.multiStandardDeviation(df)
         u = self.multiMeanCalculation(df)
-        return np.sqrt((np.log(prob*np.sqrt(2*np.pi*s*s))*-2*s*s))+u
-
+        return (np.log(prob*np.sqrt(2*np.pi*s*s))*-2*s*s)+u
